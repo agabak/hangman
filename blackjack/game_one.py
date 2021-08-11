5c09f25b6a1b9a5ea8dacddcabe19e3b4519d436
Agaba Kabunga <agaba_k@hotmail.com>
2021-08-11 08:30:44 -0500

    add black jack to the games

--- blackjack/game_one.py
+++ blackjack/game_one.py
@@ -3,17 +3,14 @@ import random
 
 user_cards = []
 computer_cards = []
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+still_play = True
 
-
-
-print(logo)
-
-def deal_card():
-    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+def deal_card(cards):
     return random.sample(cards,2)
    
-user_cards = deal_card()
-computer_cards = deal_card()
+user_cards = deal_card(cards)
+computer_cards = deal_card(cards)
 
 def  calculate_score(arry_cards):
     print(arry_cards)
@@ -29,3 +26,27 @@ computer_score = calculate_score(computer_cards)
 
 if user_score > 21:
     print("You loss")
+
+are_you_play = input("Do you want to play a game of Blackjack ? 'y' or 'n' ")
+
+if are_you_play.lower() == 'y':
+    while still_play:
+     print(logo)
+
+     print(f"Your cards : {user_cards}, current score: {user_score} \nComputer's first card : {computer_cards[0]}")
+     choose_to_play = input("Type 'y' to get another card, type 'n' to pass: ")
+
+     if choose_to_play.lower() == 'y':
+        user_cards.append(random.choice(cards))
+        computer_cards.append(random.choice(cards))
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+        
+     else:
+         still_play = False
+
+
+else:
+    print("Thank you for visiting")
+
+
