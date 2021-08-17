c7cb9f0e5afb13ab64dd376e9ff18ed74c24f9f2
Agaba Kabunga <agaba_k@hotmail.com>
2021-08-17 18:34:00 -0500

    compare number game

--- compare_game/main.py
+++ compare_game/main.py
@@ -2,13 +2,12 @@ from art import logo, vs
 from game_data import  data
 import random
 
-
+print(logo)
 def  who_have_more_follower():
     questions = random.sample(data,2)
-    print(logo)
-    print(f"Compare A: {questions[0]['name']}, {questions[0]['description']}, from {questions[0]['country']} .")
+    print(f"Compare A: {questions[0]['name']}, a {questions[0]['description']}, from {questions[0]['country']} .")
     print(vs)
-    print(f"Compare B: {questions[1]['name']}, {questions[1]['description']}, from {questions[1]['country']} .")
+    print(f"Compare B: {questions[1]['name']}, a {questions[1]['description']}, from {questions[1]['country']} .")
     return  {'A': questions[0], 'B': questions[1]}
 
 
@@ -18,12 +17,11 @@ def play_game():
     game_over = False
     while not game_over:
         result =  who_have_more_follower()
-        who_foller = input("Who has more followers ? Type 'A' or 'B'. ")
-        
-        if who_foller == 'A' and  result['A']['follower_count'] > result['B']['follower_count']:
+        who_foller = input("Who has more followers ? Type 'A' or 'B'. ").lower()
+        if who_foller == 'a' and  result['A']['follower_count'] > result['B']['follower_count']:
             score +=1
             print(result['A']['follower_count'])
-        elif who_foller == 'B' and result['B']['follower_count'] > result['A']['follower_count']:
+        elif who_foller == '' and result['B']['follower_count'] > result['A']['follower_count']:
             score +=1
             print(result['B']['follower_count'])
         else:
